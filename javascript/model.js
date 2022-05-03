bfbde48b61a5bf725b6dde8d4158f9b257b2b93f
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-05-02 23:08:17 -0300

    areeglando modal

---
+++ javascript/model.js
@@ -0,0 +1,22 @@
+// // ubicamos el icono del carrito
+// const iconCart = document.querySelector(".carro");
+// //ubico el model
+// const modal = document.querySelector(".modal");
+// //ubico el boton de cerrar
+// const cerrarModal = document.querySelector(".modalClose");
+// //abrir modal
+// const abrirModal = document.querySelector("modalOpen")
+
+
+// //cuando haga click en el icono del carro le decimos que agregue la clase "modalContainer" a la section modal (asi le agrega 1 de opacidad);
+// iconCart.addEventListener("click", (e)=>{
+//     e.preventDefault
+//     modal.classList.add("modalConteiner")
+// })
+
+// //y cuando toque el link de salir que saque esa clase y vuelva a opacidad en 0 asi no se ve
+// cerrarModal.addEventListener("click", (e)=>{
+//     e.preventDefault
+//     modal.classList.remove("modalConteiner")
+// })
+
